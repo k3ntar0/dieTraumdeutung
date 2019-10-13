1b05b1172e46cf5e0fe7e6de91307d26ad5083e4
k3ntar0 <k3ntar01wata@gmail.com>
2019-10-14 08:23:55 +0900

    Initial Commit

---
+++ dieTraumdeutung/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dieTraumdeutung
+//
+//  Created by k3ntar0 on 2019/10/14.
+//  Copyright © 2019 k3ntar0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
